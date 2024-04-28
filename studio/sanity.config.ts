55f84407ba58e38f1d7e72f2eea81cb21a21f26c
CallumHoward <7144420+CallumHoward@users.noreply.github.com>
2024-04-29 02:28:26 +1000

    Format

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,29 +1,29 @@
-import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/structure'
-import { presentationTool } from 'sanity/presentation'
-import { orderableDocumentListDeskItem } from '@sanity/orderable-document-list'
-import { FolderIcon } from '@sanity/icons'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { presentationTool } from "sanity/presentation";
+import { orderableDocumentListDeskItem } from "@sanity/orderable-document-list";
+import { FolderIcon } from "@sanity/icons";
 
-import { schemaTypes } from './schemas'
+import { schemaTypes } from "./schemas";
 
-export const projectId = process.env.SANITY_STUDIO_PROJECT_ID!
-export const dataset = process.env.SANITY_STUDIO_DATASET!
+export const projectId = process.env.SANITY_STUDIO_PROJECT_ID!;
+export const dataset = process.env.SANITY_STUDIO_DATASET!;
 
 export default defineConfig({
-  name: 'project-name',
-  title: 'Project Name',
+  name: "project-name",
+  title: "Project Name",
   projectId,
   dataset,
   plugins: [
     structureTool({
       structure: (S, context) =>
         S.list()
-          .title('Content')
+          .title("Content")
           .items([
-            S.documentTypeListItem('post'),
+            S.documentTypeListItem("post"),
             orderableDocumentListDeskItem({
-              type: 'testimonial',
-              title: 'Testimonial',
+              type: "testimonial",
+              title: "Testimonial",
               icon: FolderIcon,
               S,
               context,
@@ -33,10 +33,10 @@ export default defineConfig({
     presentationTool({
       previewUrl: {
         origin:
-          process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:5173',
+          process.env.SANITY_STUDIO_PREVIEW_URL || "http://localhost:5173",
         previewMode: {
-          enable: '/preview/enable',
-          disable: '/preview/disable',
+          enable: "/preview/enable",
+          disable: "/preview/disable",
         },
       },
     }),
@@ -44,4 +44,4 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
-})
+});
