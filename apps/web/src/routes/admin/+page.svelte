f31de5147334ac2ec008acbff353b4446401baad
CallumHoward <7144420+CallumHoward@users.noreply.github.com>
2024-06-16 17:57:02 +1000

    Add redirect to admin page

---
+++ apps/web/src/routes/admin/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { PUBLIC_SANITY_STUDIO_URL } from "$env/static/public";
+
+  onMount(() => {
+    location.href = PUBLIC_SANITY_STUDIO_URL || "http://localhost:3333";
+  });
+</script>
