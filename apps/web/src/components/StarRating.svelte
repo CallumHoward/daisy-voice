9e6aa4014182ac719923e95d14b1d9435719b54a
Tek Huynh <5714652+tekhuynh@users.noreply.github.com>
2024-07-27 14:01:46 +1000

    Doesn't work yet, but i want to stash it somewhere

--- apps/web/src/components/StarRating.svelte
+++ apps/web/src/components/StarRating.svelte
@@ -1,21 +1,40 @@
 <script lang="ts">
+  let counter: number[] = Array.from({ length: 10 }, (_, i) => i);
+  export let rating: number = 4.5;
 
-  export let rating: number = 3;
+
 </script>
 
-<label class="leading-none">
-  <div class="rating relative">
-    {#each [1, 2, 3, 4, 5] as n}
-        <p class="h-0 opacity-0 absolute">{n}</p>
+<!-- <label class="leading-none"> -->
+  <div class="rating relative rating-half">
+    {#each counter as n}
+        <!-- <p class="h-0 opacity-0 absolute">{n}</p> -->
         <input
           type="radio"
           name="rating"
-          class="mask mask-star cursor-auto bg-orange-400"
-          value={n}
+          class="mask mask-star-2 mask-half-{n % 2 + 1} cursor-auto bg-orange-400"
           bind:group={rating}
+          value={n+0.5}
           readonly
           disabled
         />
     {/each}
   </div>
-</label>
+<!-- </label> -->
+ 
+<div class="rating rating-lg rating-half">
+  <input type="radio" name="rating-10" class="rating-hidden" />
+  <input type="radio" name="rating-10" class="mask mask-star-2 mask-half-1 bg-green-500" />
+  <input type="radio" name="rating-10" class="mask mask-star-2 mask-half-2 bg-green-500" />
+  <input
+    type="radio"
+    name="rating-10"
+    class="mask mask-star-2 mask-half-1 bg-green-500" />
+  <input type="radio" name="rating-10" class="mask mask-star-2 mask-half-2 bg-green-500" />
+  <input type="radio" name="rating-10" class="mask mask-star-2 mask-half-1 bg-green-500" />
+  <input type="radio" name="rating-10" class="mask mask-star-2 mask-half-2 bg-green-500" />
+  <input type="radio" name="rating-10" class="mask mask-star-2 mask-half-1 bg-green-500" />
+  <input type="radio" name="rating-10" class="mask mask-star-2 mask-half-2 bg-green-500" />
+  <input type="radio" name="rating-10" class="mask mask-star-2 mask-half-1 bg-green-500" />
+  <input type="radio" name="rating-10" class="mask mask-star-2 mask-half-2 bg-green-500" />
+</div>
